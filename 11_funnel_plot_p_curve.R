76f39e8ed8631d0c42861ef4cc9de22ee7a4744c
Seaman <kls190004@cvltechs-MacBook-Pro.local>
2020-06-23 13:19:27 -0500

    Funnel plot versions

--- 11_funnel_plot_p_curve.R
+++ 11_funnel_plot_p_curve.R
@@ -15,13 +15,27 @@ file <- 'cor_model.RDS'
 # load data
 m.cor <- readRDS(here::here('output', file))
 
-# Contor funnel plot
+# Contor funnel plot 1
 png(file = 'figs/funnelplot.png', width = 800, height = 800) 
 meta::funnel(m.cor, xlim = c(-1.5,.5),
              contour = c(.95, .975, .99),
              col.contour=c("darkblue","blue","lightblue"))  
   legend(-1.5, 0, c("p < 0.05", "p<0.025", "< 0.01"), bty = "n",
          fill=c("darkblue","blue","lightblue"))
+dev.off() 
+
+# Contor funnel plot 2
+png(file = 'figs/funnelplot2.png', width = 800, height = 800) 
+meta::funnel(m.cor, xlim = c(-1.5,.5),
+             contour = c(.95, .975, .99),
+             col.contour=c("gray35","gray55","gray75"))  
+legend(-1.5, 0, c("p < 0.05", "p<0.025", "< 0.01"), bty = "n",
+       fill=c("gray35","gray55","gray75"))
+dev.off() 
+
+# Regular funnel plot
+png(file = 'figs/funnelplot3.png', width = 800, height = 800) 
+meta::funnel(m.cor, xlim = c(-1.5,.5))  
 dev.off() 
 
 # Eggers test ####
---
+++ scr/prisma.R
@@ -0,0 +1,14 @@
+# create prisma flow chart
+library(PRISMAstatement)
+
+prisma(found = 7561,
+       found_other = 0,
+       no_dupes = 4738, 
+       screened = 4738, 
+       screen_exclusions = 3959, 
+       full_text = 779,
+       full_text_exclusions = 749, 
+       qualitative = 30, 
+       quantitative = 30,
+       width = 800, height = 800, 
+       extra_dupes_box = TRUE)
